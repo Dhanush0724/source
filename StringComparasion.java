054791b25e87d979f2aeab286f326c7379ecfc1d
Dhanush0724 <118054875+Dhanush0724@users.noreply.github.com>
2024-02-08 20:01:48 +0530

    Add files via upload

---
+++ StringComparasion.java
@@ -0,0 +1,15 @@
+public class StringComparasion {
+    
+    public static void main(String[] args) {
+    
+     String[] Char= {"a","a","b","b","c","c","c"};
+     int n = Char.length;
+     String res[] = {};
+        while(n<Char.length){
+           if(Char[n]=="a"){
+                res +=Char[];
+
+           }
+        }
+    }
+}
